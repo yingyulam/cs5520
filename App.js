eb2487e1f5f50af86458340a99172e11eea612a1
Yingyulin <yingyulam@gmail.com>
2024-09-10 14:42:34 -0700

    Added a TextInput

--- App.js
+++ App.js
@@ -3,11 +3,19 @@ import { StyleSheet, Text, View } from 'react-native';
 import Header from "./Components/Header"
 
 export default function App() {
+  const [text, setText] = useState("");
   const appName = "My app!";
   return (
     <View style={styles.container}>
-      <Header name={appName}/>
       <StatusBar style="auto" />
+      <Header name={appName}/>
+      <TextInput >
+        placeholder="Type something"
+        autoCorrect={true} 
+        keyboardType="default"
+        value={text}
+        style={{borderBottomColor: "purple", borderBottomWidth:2}}
+      </TextInput>
     </View>
   );
 }
